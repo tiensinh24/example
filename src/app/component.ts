39be758bd5ec33bd6e89ff384c38ee735ea593fc
tiensinh24 <nguyenphucan2502@gmail.com>
2018-11-21 16:36:51 +0700

    c12

---
+++ src/app/component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+import { Model } from "./repository.model";
+
+@Component({
+    selector: "app",
+    templateUrl: "template.html"
+})
+export class ProductComponent {
+    model: Model = new Model();
+
+    getClasses(key: number): string {
+        let product = this.model.getProduct(key);
+            return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    }
+
+    getClassMap(key: number): Object {
+        let product = this.model.getProduct(key);
+        return {
+            "text-center bg-danger": product.name == "Kayak",
+            "bg-info": product.price < 50
+        };
+    }
+}
