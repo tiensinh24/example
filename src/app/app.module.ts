893eb5750636040d2b0f9659bd06fcead7011d1f
tiensinh24 <nguyenphucan2502@gmail.com>
2018-11-23 15:53:50 +0700

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { PaAttrDirective } from "./attr.directive";
 
 // import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent, PaAttrDirective
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [ProductComponent]
--- src/app/component.ts
+++ src/app/component.ts
@@ -1,6 +1,8 @@
 import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
+import { NgForm } from "@angular/forms";
+import { ProductFormGroup } from "./form.model";
 
 @Component({
     selector: "app",
@@ -8,6 +10,7 @@ import { Product } from "./product.model";
 })
 export class ProductComponent {
     model: Model = new Model();    
+    form: ProductFormGroup = new ProductFormGroup();
 
     getProduct(key: number): Product {
         return this.model.getProduct(key);
@@ -17,13 +20,24 @@ export class ProductComponent {
         return this.model.getProducts();
     }
 
-    newProduct: Product = new Product();
-
-    get jsonProduct() {
-        return JSON.stringify(this.newProduct);
-    }
+    newProduct: Product = new Product();    
 
     addProduct(p: Product) {
-        console.log("New Product: " + this.jsonProduct);
+        this.model.saveProduct(p);
+    }    
+
+    formSubmitted: boolean = false;
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.reset();
+            this.formSubmitted = false;
+        }
     }
+
+    
 }
+
